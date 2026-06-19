97f1fa4d12fa0a750f230e034ac1fc0cf7e4b627
Anantraj <anant.dhanani18@gmailcom>
2026-06-20 00:17:07 +0530

    fix namespace

--- Bulky.DataAccess/Migrations/20260508102709_SeedCategoryTable.cs
+++ Bulky.DataAccess/Migrations/20260508102709_SeedCategoryTable.cs
@@ -1,10 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
 #pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
 
-namespace BulkyBook.DataAcess.Migrations
+namespace BulkyBook.DataAccess.Migrations
 {
     /// <inheritdoc />
     public partial class SeedCategoryTable : Migration
